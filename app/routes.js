634b7d53e864bdb97cffd54cae002c4cfc2d67f1
darraghmckeogh <darragh.mckeogh@paconsulting.com>
2023-01-17 13:15:34 +0000

    new

--- app/routes.js
+++ app/routes.js
@@ -20,7 +20,7 @@ router.post('/returning-boolean-answer', function (req, res) {
       res.redirect('/returning-code')
     } else {
       // Send user to ineligible page
-      res.redirect('/questions/building-name')
+      res.redirect('/questions/building/building-name')
     }
   
   })
@@ -43,6 +43,23 @@ router.post('/returning-code-answer', function (req, res) {
   })
 
 // Run this code when a form is submitted to number-of-residential-units-answer
+router.post('/other-building-name-boolean-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'returning-code'
+  var otherBuildingName = req.session.data['other-building-name-boolean']
+
+  // Check whether the variable matches a condition
+  if (otherBuildingName == "Yes"){
+    // Send user to next page
+    res.redirect('/questions/building/other-building-name')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/questions/contact-details/name')
+  }
+
+})
+
+// Run this code when a form is submitted to number-of-residential-units-answer
 router.post('/residential-units-answer', function (req, res) {
 
   // Make a variable and give it the value from 'returning-code'
@@ -51,7 +68,7 @@ router.post('/residential-units-answer', function (req, res) {
   // Check whether the variable matches a condition
   if (Number(residentialUnits) < 2){
     // Send user to next page
-    res.redirect('/check-answers-eligibility')
+    res.redirect('/questions/blocks/add-block')
   } else {
     // Send user to ineligible page
     res.redirect('/questions/blocks/are-people-living-in-the-building')
@@ -70,13 +87,30 @@ router.post('/eligibility-answer', function (req, res) {
   
     if ((Number(numberOfFloors) >= 7 || Number(buildingHeight) >= 18) && Number(residentialUnits) >= 2 && occupied !== "No and people will not be moving in"){
       // Send user to eligible page
-      res.redirect('/questions/contact-details/email')
+      res.redirect('/questions/blocks/completion-year')
     } else {
         // Send user to not eligible page
-        res.redirect('/not-eligible')
+        res.redirect('/questions/blocks/add-block')
     }
   
   })
+
+  // Run this code when a form is submitted to number-of-residential-units-answer
+router.post('/completion-year-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'returning-code'
+  var completionYear = req.session.data['completion-year-radio']
+
+  // Check whether the variable matches a condition
+  if (completionYear == "The block has not yet been completed") {
+    // Send user to next page
+    res.redirect('/questions/blocks/add-block')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/questions/blocks/completion-certificate-issuer')
+  }
+
+})
 
   // Run this code when a form is submitted to other-name-address-postcode-boolean-answer
 router.post('/other-name-address-postcode-boolean-answer', function (req, res) {
@@ -87,10 +121,27 @@ router.post('/other-name-address-postcode-boolean-answer', function (req, res) {
     // Check whether the variable matches a condition
     if (otherNameAddressPostcode == "Yes"){
       // Send user to next page
-      res.redirect('/questions/building/other-name-address-postcode')
+      res.redirect('/questions/blocks/other-name-address-postcode')
     } else {
       // Send user to ineligible page
-      res.redirect('/questions/building/completion-year')
+      res.redirect('/questions/blocks/add-block')
     }
   
   })
+
+// Run this code when a form is submitted to returning-boolean-answer
+router.post('/add-block-boolean-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'returning-boolean'
+  var addBlock = req.session.data['add-block-boolean']
+
+  // Check whether the variable matches a condition
+  if (addBlock == "Yes"){
+    // Send user to next page
+    res.redirect('/questions/blocks/floors-above')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/check-answers-blocks')
+  }
+
+})
